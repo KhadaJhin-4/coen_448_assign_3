d76bec9b196a8ff224328c8f9230fcd839af5d01
Keeano Gerald <keeanogerald@gmail.com>
2023-04-20 03:45:43 -0400

    added selectTestOutofIndex

--- Test/Test2Way.java
+++ Test/Test2Way.java
@@ -1,10 +1,11 @@
+import edu.princeton.cs.algs4.Quick;
 import org.junit.*;
 import edu.princeton.cs.algs4.StdIn;
 import edu.princeton.cs.algs4.StdOut;
 import edu.princeton.cs.algs4.StdRandom;
+import org.junit.jupiter.api.function.Executable;
 
-import static org.junit.jupiter.api.Assertions.assertArrayEquals;
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 
 public class Test2Way {
@@ -16,10 +17,22 @@ public class Test2Way {
         assertEquals(4,j);
     }
 
+    @Test
+    public void selectTestOutofIndex()
+    {
+       assertThrows(IllegalArgumentException.class, new Executable() {
+           @Override
+           public void execute() throws Throwable {
+               Comparable[] a = {1,2,3,4,5,6,7};
+               Comparable j = Quick.select(a,-2);
+           }
+       }) ;
+    }
+
 
     @Test
     public void isSortedTest()
     {
-
+        //Quick.sort(a);
     }
 }
